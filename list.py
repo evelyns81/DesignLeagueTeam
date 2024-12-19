b0276bcf3bcb80526d57ffe9e0983bc637628af6
E Say <144722344+evelyns81@users.noreply.github.com>
2024-12-19 12:46:38 -0500

    Create list.py

---
+++ list.py
@@ -0,0 +1,24 @@
+#Evelyn say 
+#5-6
+#12/19/24
+
+favorite=["black","0.10","soup"]
+print(favorite[1])
+
+groceries=["Eggs","apples","milk","bread","jucie"]
+print(groceries[4])
+
+people=["Marisa","hailey","nibrus","arafath","neelam","harbans","grace","farhan","sherana","amelia","edwin","sarika","romina","malachi","syeda","angle","sheldon","mia","emirah","William","Aarya","Cesar"]
+print(people[0:3])
+for person in people:
+    print("This is my friend " + person)
+    
+    
+donatiton = ["cookies","sugar cookies","cupcakes","chips","cookies","scooby snacks","sour patch","broke","utensils","pretzels","soda","not sure","chips","drinks","choc chips cookies","cookies","food",]
+
+for i in range(19):
+    print(f"{people[i]} will bring {donatiton[i]}.")
+    
+    
+    
+
